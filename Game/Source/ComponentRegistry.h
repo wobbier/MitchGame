4a908e9d75e02b80bea56cfac6e2a21bfe58772f
Mitch Andrews <rastaninja77@gmail.com>
2021-04-15 01:52:50 -0400

    🧼 Example scene

--- Game/Source/ComponentRegistry.h
+++ Game/Source/ComponentRegistry.h
@@ -3,5 +3,7 @@
 #include "Cores/TestCore.h"
 #include "Cores/SceneGraphTestCore.h"
 #include "Cores/PhysicsTest.h"
+#include "Cores/CharacterCore.h"
 
 #include "Components/MenuController.h"
+#include "Components/Character.h"
